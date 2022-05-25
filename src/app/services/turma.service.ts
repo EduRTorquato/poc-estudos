9351a05831aace9f78b1877d88d74217c68db7e4
ertorquato <ertorquato@stefanini.com>
2022-05-25 16:00:49 -0300

    atualização

--- src/app/services/turma.service.ts
+++ src/app/services/turma.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Turma } from 'src/models/turma';
 
 @Injectable({
@@ -8,11 +8,16 @@ import { Turma } from 'src/models/turma';
 })
 export class TurmaService {
 
-  constructor(private _httclient: HttpClient) { }
-  private url=" http://localhost:3000/turmas"
+  constructor(private _httpclient: HttpClient) { }
+  private url= "http://localhost:3000/turmas"
   
 
   getTurmas(): Observable<Turma[]>{
-    return this._httclient.get<Turma[]>(this.url)
+    return this._httpclient.get<Turma[]>(this.url)
   }
+
+  postTurma(turma: Turma){
+    return this._httpclient.post(this.url, turma);    
+  }
+
 }
